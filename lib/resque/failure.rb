3b5299d8baf951a7ff604560146d01c6e568b0b8
Brian P O'Rourke <bpo@somnambulance.net>
2009-10-29 16:18:49 -0700

    Support for automatic retry of jobs.
    Auto-retry is an alternative failure system that can be backed by any
    other failure system once the specified number of attempts has
    expired.

--- lib/resque/failure.rb
+++ lib/resque/failure.rb
@@ -1,32 +1,15 @@
+require 'resque/failure/backend_support'
+
 module Resque
   module Failure
-    def self.create(options = {})
-      backend.new(*options.values_at(:exception, :worker, :queue, :payload)).save
-    end
 
     ##
     # require 'resque/failure/hoptoad'
     # Resque::Failure.backend = Resque::Failure::Hoptoad
-    def self.backend=(backend)
-      @backend = backend
-    end
+    extend BackendSupport
 
-    def self.backend
-      return @backend if @backend
-      require 'resque/failure/redis'
-      @backend = Failure::Redis
-    end
-
-    def self.count
-      backend.count
-    end
-
-    def self.all(start = 0, count = 1)
-      backend.all(start, count)
-    end
-
-    def self.url
-      backend.url
+    def self.create(options = {})
+      backend.new(*options.values_at(:exception, :worker, :queue, :payload)).save
     end
   end
 end
